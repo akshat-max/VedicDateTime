d247a1eca1cff5d0ccf2ced1a249aa9b313458cf
Akshat Mittal <akshat.mittal2930@gmail.com>
2022-04-16 20:11:19 +0530

    file added

---
+++ PowerR/R/power.R
@@ -0,0 +1,49 @@
+#' Square a number
+#' 
+#' Takes in any numeric value and square it.
+#' Akshat Mittal
+#' @param x A numeric value to be squared 
+#' @export 
+#' @return the square of input
+square<- function(x){
+  return(x^2)
+}
+
+#' cube a number
+#' 
+#' Takes in any numeric value and cube it.
+#' @param x A numeric value to be cubed 
+#' @export 
+#' @return the cube of input 
+cube<- function(x){
+return(x^3)
+}
+
+#' biquadrates a number
+#' 
+#' Takes in any numeric value and biquadrates it.
+#' @param x A numeric value to be biquadrates 
+#' @export 
+#' @return the biquadrates of input 
+biquadrates<- function(x){
+  return(x^4)
+}
+#' fifth power a number
+#' 
+#' Takes in any numeric value and fifth power it.
+#' @param x A numeric value to be fifth power 
+#' @export 
+#' @return the fifth power of input 
+fifth_power<-function(x){
+  return(x^5)
+}
+#' sixth power a number
+#' 
+#' Takes in any numeric value and sixth power it.
+#' @param x A numeric value to be sixth power 
+#' @export 
+#' @return the sixth power of input 
+sixth_power<-function(x){
+  return(x^6)
+}
+
---
+++ PowerR/README.md
@@ -0,0 +1,23 @@
+# PowerR
+**R** package implementing a workflow for GitHub Actions using [r-lib/actions](https://github.com/r-lib/actions).
+
+
+## Build and Installation
+
+
+### Windows
+```sh
+Rscript.exe -e 'devtools::install_github("akshat-max/powerR")'
+```
+
+## GitHub Actions Workflow
+- Checks for a successful build on Windows. </br>
+  **Using:**
+    - r-lib/actions/setup
+    - r-lib/actions/setup-r-dependencies
+- Checks for a errors in the R package. </br>
+  **Using:**
+    - r-lib/actions/check-r-package
+
+
+Examples for this package can be located in the [example](https://github.com/akshat-max/PowerR/tree/main/example) directory. 
